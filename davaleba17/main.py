48ad48f20fc36c7a08b10025319f3864d5951942
natiaker <n.kerdzaia@gmail.com>
2024-02-14 19:13:56 +0400

    add sell method to House class and sell 2 houses

--- davaleba17/main.py
+++ davaleba17/main.py
@@ -5,7 +5,7 @@ class Person:
         self.loan = loan
 
     def __str__(self):
-        return f"Person სახელი: {self.name}, დეპოზიტი: {self.deposit}, სესხი: {self.loan}"
+        return f"სახელი: {self.name}, დეპოზიტი: {self.deposit}, სესხი: {self.loan}"
 
 
 class House:
@@ -15,7 +15,23 @@ class House:
         self.owner = owner
         self.status = status
 
+    def sell(self, buyer, loan_amount=0):
+        if buyer.deposit >= self.price:
+            self.owner.deposit += self.price
+            self.owner = buyer.name
+            buyer.deposit -= self.price
+            self.status = "გაყიდულია"
+            print(f"{self.ID} ახალი მეპატრონე: ({buyer})")
+        else:
+            print("არ არის საკმარისი თანხა")
+
 
 house_owner = Person("John", 5000, 70000)
-house_buyer = Person("Mary", 200000, 5000)
-house = House(123456, 120000, owner=house_owner)
+house_buyer1 = Person("Mary", 200000)
+house1 = House(123456, 120000, house_owner)
+house1.sell(house_buyer1)
+house_buyer2 = Person("Henry", 3000)
+house2 = House(789997, 100000, house_owner)
+house2.sell(house_buyer2)
+print(f"ბინების მეპატრონე: {house_owner}")
+

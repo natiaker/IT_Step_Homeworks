ba5b861d05f11fb0ed3aca86cfa0926bddc5287f
natiaker <n.kerdzaia@gmail.com>
2024-02-14 19:52:41 +0400

    fix house sell

--- davaleba17/main.py
+++ davaleba17/main.py
@@ -18,20 +18,29 @@ class House:
     def sell(self, buyer, loan_amount=0):
         if buyer.deposit >= self.price:
             self.owner.deposit += self.price
-            self.owner = buyer.name
+            self.owner = buyer
             buyer.deposit -= self.price
-            self.status = "გაყიდულია"
-            print(f"{self.ID} ახალი მეპატრონე: ({buyer})")
+            self.status = "გაყიდული"
+            print(f"{self.ID} ბინა - {self.status}. ახალი მეპატრონე: ({buyer})")
+        elif loan_amount > self.price:
+            self.owner.deposit += self.price
+            self.owner = buyer
+            self.status = "გაყიდული სესხით"
+            buyer.loan += loan_amount
+            print(f"{self.ID} ბინა - {self.status}. ახალი მეპატრონე: ({buyer})")
         else:
-            print("არ არის საკმარისი თანხა")
+            print(f"{buyer.name}ს დეპოზიტზე არ არის საკმარისი თანხა. ({buyer})")
 
 
-house_owner = Person("John", 5000, 70000)
-house_buyer1 = Person("Mary", 200000)
+house_owner = Person("ჯონი", 5000, 70000)
+
+house_buyer1 = Person("მარი", 200000)
 house1 = House(123456, 120000, house_owner)
 house1.sell(house_buyer1)
-house_buyer2 = Person("Henry", 3000)
+
+house_buyer2 = Person("გურამი", 25000, 2000)
 house2 = House(789997, 100000, house_owner)
-house2.sell(house_buyer2)
-print(f"ბინების მეპატრონე: {house_owner}")
+house2.sell(house_buyer2, 120000)
+
+print(f"ბინების მეპატრონე: ({house_owner})")
 

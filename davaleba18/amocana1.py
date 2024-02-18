b3049418cd9b9d8209dfb3d4c62021b049066d89
natiaker <n.kerdzaia@gmail.com>
2024-02-18 15:18:21 +0400

    add decorator

--- davaleba18/amocana1.py
+++ davaleba18/amocana1.py
@@ -1,5 +1,16 @@
+def check_num(func):
+    def wrapper(*args, **kwargs):
+        if args[0] > 0:
+            return func(args[0])
+        else:
+            raise ValueError('Argument is not positive')
+    return wrapper
+
 
 @check_num
 def return_arg(num):
     return num
 
+
+print(return_arg(6))
+print(return_arg(-7))

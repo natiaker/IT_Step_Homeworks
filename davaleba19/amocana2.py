bf625e2b3708d56087368d1ab1e67541d0c553b7
natiaker <n.kerdzaia@gmail.com>
2024-02-21 17:28:56 +0400

    create node class

---
+++ davaleba19/amocana2.py
@@ -0,0 +1,6 @@
+# Create a Node class
+class Node:
+    def __init__(self, data):
+        # Initialize object with given data and info about next object
+        self.data = data
+        self.next = None

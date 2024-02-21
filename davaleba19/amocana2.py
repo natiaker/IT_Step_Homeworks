1d3af277b9f2ac555e8c331aa02bd5770fb2a3d6
natiaker <n.kerdzaia@gmail.com>
2024-02-21 17:55:37 +0400

    add remove method using data/value

--- davaleba19/amocana2.py
+++ davaleba19/amocana2.py
@@ -30,6 +30,29 @@ class LinkedList:
         # When last node's next is None we add new node
         last_node.next = new_node
 
+    # Remove using passed value/data
+    def remove(self, data):
+        # If list is empty we have nothing to remove
+        if self.head is None:
+            return
+
+        # If the head node itself holds the value to be remooved
+        if self.head.data == data:
+            self.head = self.head.next  # head element is going to be next element
+            return
+
+        # Start from the head of the linked list
+        current_node = self.head
+        # Traverse the linked list to find the node with the data value to be deleted
+        while current_node.next:
+            if current_node.next.data == data:
+                current_node.next = current_node.next.next
+                return
+            current_node = current_node.next
+
+        # If data is not found in the linked list
+        print("Data not found in the linked list")
+
     def display_info(self):
         # Start from the head of the linked list
         current_node = self.head
@@ -49,3 +72,11 @@ linked_list.append(6)
 linked_list.append(7)
 linked_list.append(8)
 linked_list.display_info()
+# Remove some elements using data value
+linked_list.remove(7)
+linked_list.display_info()
+linked_list.remove(5)
+linked_list.display_info()
+linked_list.remove(2)
+linked_list.display_info()
+

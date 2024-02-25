d1670da51950314d6bd35d3df754a5cb7f59e074
natiaker <n.kerdzaia@gmail.com>
2024-02-25 12:36:24 +0400

    create function to print leaf nodes

--- davaleba20/main.py
+++ davaleba20/main.py
@@ -7,3 +7,26 @@ class Node:
         self.right = None
 
 
+# Function to print leaf nodes
+def printLeafNodes(node):
+    # If node is none return
+    if node is None:
+        return
+    # If node has no child - left and right - nodes, it is a leaf node
+    if node.left is None and node.right is None:
+        print(f"Leaf node: {node.value}")
+    # Call recursion for left and right child nodes
+    printLeafNodes(node.left)
+    printLeafNodes(node.right)
+
+
+# Create a binary tree
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+root.right.left.left = Node(8)
+printLeafNodes(root)

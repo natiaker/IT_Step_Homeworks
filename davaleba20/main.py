20f549ad60c44224db253a11d0a12ed039656847
natiaker <n.kerdzaia@gmail.com>
2024-02-25 11:38:37 +0400

    create Node class

---
+++ davaleba20/main.py
@@ -0,0 +1,9 @@
+# Create a node class
+class Node:
+    # Initialize object with given value, and left child and right child
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+

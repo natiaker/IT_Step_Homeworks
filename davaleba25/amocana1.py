77716f8569241ae30254a19105cb6c907652ba1e
natiaker <n.kerdzaia@gmail.com>
2024-03-22 16:20:05 +0400

    add comments to amocana1

--- davaleba25/amocana1.py
+++ davaleba25/amocana1.py
@@ -2,6 +2,7 @@ import json
 import threading
 
 
+# function to read and print json data from files
 def read_json(file_name):
     with open(file_name, 'r') as json_file:
         json_data = json.load(json_file)
@@ -14,11 +15,13 @@ json_list = ["file1.json", "file2.json", "file3.json"]
 
 
 thread_list = []
+# create and start threads for each json file
 for file in json_list:
     thread = threading.Thread(target=read_json, args=(file,))
     thread.start()
     thread_list.append(thread)
 
+# wait for all threads to finish
 for thread in thread_list:
     thread.join()
 

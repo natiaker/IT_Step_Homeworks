2c18e6c3d0aa625349b8de62a548af3d4ba7b435
natiaker <n.kerdzaia@gmail.com>
2024-05-07 14:03:23 +0400

    add edit functionality

--- davaleba35/app.py
+++ davaleba35/app.py
@@ -52,6 +52,21 @@ def delete(id):
     return redirect(url_for('index'))
 
 
+@app.route('/update/<id>', methods=['POST'])
+def update(id):
+    car = Car.query.get(id)
+
+    if car:
+        if request.method == "POST":
+            car.manufacturer = request.form['manufacturer']
+            car.model = request.form['model']
+            car.instock = request.form['instock']
+            car.price = request.form['price']
+
+            db.session.commit()
+        return redirect(url_for('index'))
+
+
 if __name__ == "__main__":
 
     with app.app_context():
